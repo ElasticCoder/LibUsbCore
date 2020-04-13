2c1e118d2ca168be035ad8157f7d977e8dadea83
Andrew Potts <github@andypotts.com>
2020-04-13 08:55:52 +0100

    Initial check-in

---
+++ TestConsole/Program.cs
@@ -0,0 +1,34 @@
+﻿using LibUsbDotNet.LibUsb;
+using System;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                UsbContext context = new UsbContext();
+                var devices = context.List();
+
+                Console.WriteLine("Registered USB devices:");
+                foreach (var device in devices)
+                {
+                    Console.WriteLine(device.ToString());
+                }
+
+                Console.WriteLine();
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine($"An {error.GetType()} occurred:");
+                while (error != null)
+                {
+                    Console.WriteLine(error.Message);
+                    error = error.InnerException;
+                }
+            }
+        }
+    }
+}
